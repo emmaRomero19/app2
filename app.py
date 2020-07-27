fa30c5e1c4a010e302f3e6d83a90467e35dabbdf
Emma Romero <emmayrs17@gmail.com>
2020-07-27 14:02:10 -0500

    muestra los datos, pero no recibe en update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ urls = (
     '/insert', 'mvc.controllers.alumnos.insert.Insert',
     '/delete/?', 'mvc.controllers.alumnos.delete.Delete',
     '/view/?', 'mvc.controllers.alumnos.viewOne.View',
-    '/update/?', 'mvc.controllers.alumnos.update.Update',
+    '/update', 'mvc.controllers.alumnos.update.Update',
 )
 app = web.application(urls, globals())
 
--- mvc/controllers/alumnos/update.py
+++ mvc/controllers/alumnos/update.py
@@ -4,16 +4,24 @@ render=web.template.render('mvc/views/alumnos/')
 class Update:
     def GET(self):
         try: 
+            return render.update()
+        except Exception as e:
+            result=[]    
+            result.append('error'+ str(e.args))
+            return result
+
+    def POST(self):
+        try:
             data=web.input()
-            matricula=data['matricula']
-            name=data['name']
-            paterno=data['paterno']
-            materno=data['materno']
-            edad=data['edad']
-            fecha=data['fecha']
-            genero=data['genero']
-            state=data['state']
-            return render.update(matricula,name,paterno,materno,edad,fecha,genero,state)
+            matricula=int(data.matricula)
+            name=str(data.name)
+            paterno=str(data.paterno)
+            materno=str(data.materno)
+            edad=int(data.edad)
+            fecha=str(data.fecha)
+            genero=str(data.genero)
+            state=str(data.state)
+            return render.list(matricula,name,paterno,materno,edad,fecha,genero,state)
         except Exception as e:
             result=[]    
             result.append('error'+ str(e.args))
