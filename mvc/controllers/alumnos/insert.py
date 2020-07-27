ce036bc90c8785717f7615cdfc33447c02b3c597
Emma Romero <emmayrs17@gmail.com>
2020-07-27 10:22:02 -0500

    se puede insertar informacion y mostrarla en la lista

--- mvc/controllers/alumnos/insert.py
+++ mvc/controllers/alumnos/insert.py
@@ -9,3 +9,19 @@ class Insert:
             result=[]    
             result.append('error'+ str(e.args))
             return result
+    def POST(self):
+        try:
+            data=web.input()
+            matricula=int(data.matricula)
+            name=str(data.name)
+            paterno=str(data.paterno)
+            materno=str(data.materno)
+            edad=int(data.edad)
+            fecha=str(data.fecha)
+            genero=str(data.genero)
+            state=str(data.state)
+            return render.list(matricula,name,paterno,materno,edad,fecha,genero,state)
+        except Exception as e:
+            result=[]    
+            result.append('error'+ str(e.args))
+            return result
